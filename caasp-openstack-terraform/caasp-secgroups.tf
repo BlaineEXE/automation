fdbcf7f5af10505d970d2a036d163fd56d7760b5
Blaine Gardner <blaine.gardner@suse.com>
2018-06-06 10:55:44 -0600

    os-terraform: Move secgroups to new file
    The single caasp-cluster.tf file is a bit too large to navigate easily.
    Separate secgroups into new file to improve this a lot.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

---
+++ caasp-openstack-terraform/caasp-secgroups.tf
@@ -0,0 +1,177 @@
+resource "openstack_compute_secgroup_v2" "secgroup_base" {
+  name        = "caasp-base"
+  region      = "${var.region_name}"
+  description = "Basic security group for CaaSP"
+
+  rule {
+    from_port   = -1
+    to_port     = -1
+    ip_protocol = "icmp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 2379
+    to_port     = 2379
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8472
+    to_port     = 8472
+    ip_protocol = "udp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "secgroup_admin" {
+  name        = "caasp-admin"
+  region      = "${var.region_name}"
+  description = "CaaSP security group for admin"
+
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 443
+    to_port     = 443
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 4505
+    to_port     = 4506
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 389
+    to_port     = 389
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "secgroup_master" {
+  name        = "caasp-master"
+  region      = "${var.region_name}"
+  description = "CaaSP security group for masters"
+
+  rule {
+    from_port   = 2380
+    to_port     = 2380
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 6443
+    to_port     = 6444
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8285
+    to_port     = 8285
+    ip_protocol = "udp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 30000
+    to_port     = 32768
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 30000
+    to_port     = 32768
+    ip_protocol = "udp"
+    cidr        = "0.0.0.0/0"
+  }
+}
+
+resource "openstack_compute_secgroup_v2" "secgroup_worker" {
+  name        = "caasp-worker"
+  region      = "${var.region_name}"
+  description = "CaaSP security group for workers"
+
+  rule {
+    from_port   = 80
+    to_port     = 80
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 443
+    to_port     = 443
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8080
+    to_port     = 8080
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8081
+    to_port     = 8081
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 2380
+    to_port     = 2380
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 10250
+    to_port     = 10250
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8285
+    to_port     = 8285
+    ip_protocol = "udp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 30000
+    to_port     = 32768
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 30000
+    to_port     = 32768
+    ip_protocol = "udp"
+    cidr        = "0.0.0.0/0"
+  }
+}
