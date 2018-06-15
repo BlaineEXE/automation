a57beafba93c1982f54eca7c410f2fe17d44497e
Blaine Gardner <blaine.gardner@suse.com>
2018-06-15 14:56:49 -0600

    os-terraform: Default to QA team's hostnames
    Change the default cluster name to make no changes by default to the QA
    team's hostnames.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@suse.com>

--- caasp-openstack-terraform/caasp-secgroups.tf
+++ caasp-openstack-terraform/caasp-secgroups.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_secgroup_v2" "secgroup_base" {
-  name        = "${var.identifier}-caasp-base"
+  name        = "${var.identfier}caasp-base"
   description = "Basic security group for CaaSP"
 
   rule {
@@ -32,7 +32,7 @@ resource "openstack_compute_secgroup_v2" "secgroup_base" {
 }
 
 resource "openstack_compute_secgroup_v2" "secgroup_admin" {
-  name        = "${var.identifier}-caasp-admin"
+  name        = "${var.identfier}caasp-admin"
   description = "CaaSP security group for admin"
 
   rule {
@@ -79,7 +79,7 @@ resource "openstack_compute_secgroup_v2" "secgroup_admin" {
 }
 
 resource "openstack_compute_secgroup_v2" "secgroup_master" {
-  name        = "${var.identifier}-caasp-master"
+  name        = "${var.identfier}caasp-master"
   description = "CaaSP security group for masters"
 
   rule {
@@ -119,7 +119,7 @@ resource "openstack_compute_secgroup_v2" "secgroup_master" {
 }
 
 resource "openstack_compute_secgroup_v2" "secgroup_worker" {
-  name        = "${var.identifier}-caasp-worker"
+  name        = "${var.identfier}caasp-worker"
   description = "CaaSP security group for workers"
 
   rule {
